1015c78f7980a878b254b824e6a463238f5788c2
jnh277 <johannes.n.hendriks@gmail.com>
2020-09-24 14:52:57 +1000

    Update scalar_arx_example.py

--- scalar_arx_example.py
+++ scalar_arx_example.py
@@ -1,8 +1,14 @@
+# uses an arx model structure to learn a dynamic systems output probability density function
+# this example is for a very simple no input single output feedback system
+# what is interesting is that it can learn different pdfs: gaussian, cauchy, bimodal gaussian
+
 import torch
 import matplotlib.pyplot as plt
 import torch.utils.data as data
 import Models
 import numpy as np
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")      # use gpu if available
 
 N = 4000
 batch_size = 64
@@ -14,8 +20,7 @@ stds = torch.zeros((1, 2))
 stds[0, 0] = 0.4
 stds[0, 1] = 0.6
 
-
-
+noise_form = 'cauchy'            # this can also be 'gaussian', or 'bimodal'
 
 # simulate a really simple arx system
 a = 0.95
@@ -26,23 +31,27 @@ y0 = 2
 y = torch.empty((N,))
 y[0] = y0
 
-# bimodal noise
-# for i in range(N-1):
-#     # y[i+1] = a*y[i]+ sig_m*torch.randn((1,))
-#     if torch.rand((1,)) > 0.5:
-#         y[i + 1] = a * y[i] + 0.2 + sig_m * torch.randn((1,))
-#     else:
-#         y[i + 1] = a * y[i] - 0.2 + sig_m * torch.randn((1,))
 
-# cauchy noise
-for i in range(N-1):
-    y[i+1] = a*y[i]+ sig_m*torch.from_numpy(np.random.standard_t(1, (1,)))
+if noise_form == 'bimodal': # bimodal noise
+    for i in range(N-1):
+        # y[i+1] = a*y[i]+ sig_m*torch.randn((1,))
+        if torch.rand((1,)) > 0.5:
+            y[i + 1] = a * y[i] + 0.2 + sig_m * torch.randn((1,))
+        else:
+            y[i + 1] = a * y[i] - 0.2 + sig_m * torch.randn((1,))
+elif noise_form == 'cauchy': # cauchy noise
+    for i in range(N-1):
+        y[i+1] = a*y[i]+ sig_m*torch.from_numpy(np.random.standard_t(1, (1,)))
 
+elif noise_form == 'gaussian': # gaussian
+    for i in range(N-1):
+        y[i+1] = a*y[i]+ sig_m*torch.from_numpy(np.random.normal((1,)))
 
 # normalise the data
-y = y/y.max()
+y = y/2
 
 plt.hist(y[1:]-a*y[:-1],bins=30, range=(-.5,0.5))
+plt.title('noise distribution')
 plt.show()
 
 # convert into ML inputs and outputs
@@ -55,7 +64,11 @@ dataset = data.TensorDataset(X,Y)
 train_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
 
 
+
+
 network = Models.ScalarNet(hidden_dim=50)
+
+network.to(device)
 
 optimizer = torch.optim.Adam(network.parameters(), lr=learning_rate)
 
@@ -65,21 +78,21 @@ for epoch in range(num_epochs):
     network.train()  # (set in training mode, this affects BatchNorm and dropout)
     batch_losses = []
     for step, (xs, ys) in enumerate(train_loader):
-        xs = xs.unsqueeze(1)  # (shape: (batch_size, 1))
-        ys = ys.unsqueeze(1)  # (shape: (batch_size, 1))
+        xs = xs.unsqueeze(1).to(device)  # (shape: (batch_size, 1))
+        ys = ys.unsqueeze(1).to(device)  # (shape: (batch_size, 1))
 
         x_features = network.feature_net(xs)  # (shape: (batch_size, hidden_dim))
         scores_gt = network.predictor_net(x_features, ys)  # (shape: (batch_size, 1))
         scores_gt = scores_gt.squeeze(1)  # (shape: (batch_size))
 
         y_samples_zero, q_y_samples, q_ys = Models.sample_gmm_centered(stds, num_samples=num_samples)
-        # y_samples_zero = y_samples_zero.cuda()  # (shape: (num_samples, 1))
+        y_samples_zero = y_samples_zero.to(device)  # (shape: (num_samples, 1))
         y_samples_zero = y_samples_zero.squeeze(1)  # (shape: (num_samples))
-        # q_y_samples = q_y_samples.cuda()  # (shape: (num_samples))
+        q_y_samples = q_y_samples.to(device)  # (shape: (num_samples))
         y_samples = ys + y_samples_zero.unsqueeze(0)  # (shape: (batch_size, num_samples))          # uncenters
         q_y_samples = q_y_samples.unsqueeze(0) * torch.ones(y_samples.size())  # (shape: (batch_size, num_samples))
         q_ys = q_ys[0] * torch.ones(xs.size(0))  # (shape: (batch_size))
-
+        q_ys = q_ys.to(device)
         scores_samples = network.predictor_net(x_features, y_samples)  # (shape: (batch_size, num_samples))
 
         ########################################################################
@@ -104,6 +117,8 @@ for epoch in range(num_epochs):
     epoch_loss = np.mean(batch_losses)
     epoch_losses_train.append(epoch_loss)
     print('Epoch: {0} train loss: {1}'.format(epoch,epoch_loss))
+
+network.cpu()
 
 plt.plot(epoch_losses_train)
 plt.show()
