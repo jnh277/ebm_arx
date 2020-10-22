d9ef1e0e2d2074b8b473322b2a12149c99ad3c22
johannes <johannes.n.hendriks@gmail.com>
2020-10-23 10:22:58 +1100

    Updates arx example to include lsqr baseline

--- arx_example.py
+++ arx_example.py
@@ -1,25 +1,44 @@
-# η = {a1:na , b0:nb , σe}. = {−1.5,0.7,0,1,0.5,1}
-# {0.33,0.98,0.98,0.33,1.22,0.56,−0.18,0.5}
-# {0.02, 0.04, 0.02, −1.56, 0.64, 0.05}
+# data generated using a second order ARX model
 
 import numpy as np
 import torch
 import torch.utils.data as data
 import matplotlib.pyplot as plt
 import Models
+import scipy.linalg as linalg
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")      # use gpu if available
 
 
 class GenerateARXData(object):
-    def __init__(self, y0=[0,0], sd_u=0.1, sd_y=0.3):
+    def __init__(self, y0=[0,0], sd_u=0.1, sd_y=0.3, noise_form='gaussian'):
         self.sd_u = sd_u
         self.sd_v = sd_y
         self.y0 = y0
+        self.noise_form = noise_form
 
     def _generate_random_input(self, n):
         u = np.random.normal(0, self.sd_u, (n,1))
         return u
+
+    def _noise_model(self):
+        if self.noise_form == 'clipped_gaussian':
+            e = max(min(self.sd_v*1.5,self.sd_v * np.random.randn()),-self.sd_v*1.5)
+        elif self.noise_form == 'bimodal':
+            if np.random.uniform() > 0.5:
+                e = self.sd_v * np.random.randn() + self.sd_v*2
+            else:
+                e = self.sd_v * np.random.randn() - self.sd_v*2
+        elif self.noise_form == 'upper_clipped_gaussian':
+            e = min(self.sd_v*1.5,self.sd_v * np.random.randn())
+        elif self.noise_form == 'half_gaussian':
+            e = abs(self.sd_v * np.random.randn())
+        elif self.noise_form == 'cauchy':
+            e = self.sd_v * torch.from_numpy(np.random.standard_t(1, (1,)))
+        else:
+            e = self.sd_v * np.random.randn()
+
+        return e
 
     def _linear_function(self, y1, y2, u1, u2):
         return (1.5* y1 - 0.7 * y2 + u1 + 0.5 * u2)
@@ -31,7 +50,7 @@ class GenerateARXData(object):
         y[1] = self.y0[1]
 
         for k in range(2, n):
-            e[k] = self.sd_v * np.random.randn()
+            e[k] = self._noise_model()
             y[k] = self._linear_function(y[k - 1], y[k - 2], u[k - 1], u[k - 2]) + e[k]
         return y, e
 
@@ -54,7 +73,8 @@ class GenerateARXData(object):
 
 # ---- Main script ----
 if __name__ == "__main__":
-    N = 6000
+    N = 1000
+    N_test = 200
     batch_size = 128
     learning_rate = 0.001
     num_samples = 512
@@ -62,19 +82,22 @@ if __name__ == "__main__":
     stds = torch.zeros((1, 2))
     stds[0, 0] = 0.2
     stds[0, 1] = 0.4
+    noise_form = 'gaussian'
 
-    dataGen = GenerateARXData()
+    dataGen = GenerateARXData(noise_form=noise_form)
     X, Y, E = dataGen(N, 1)
 
     # Normalise the data
-    X = torch.from_numpy(X / X.std(0)).float()
-    Y = torch.from_numpy(Y / Y.std(0)).float()
+    scale = Y.std(0)
+    X = torch.from_numpy(X / scale).float()
+    Y = torch.from_numpy(Y / scale).float()
+    E = E/scale
 
     dataset = data.TensorDataset(X, Y)
     train_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
 
 
-    network = Models.ARXnet(x_dim=4,y_dim=1,hidden_dim=50)
+    network = Models.ARXnet(x_dim=4,y_dim=1,hidden_dim=100)
     network.to(device)
     optimizer = torch.optim.Adam(network.parameters(), lr=learning_rate)
 
@@ -108,6 +131,8 @@ if __name__ == "__main__":
     network.cpu()
 
     plt.plot(epoch_losses_train)
+    plt.title('Training loss')
+    plt.xlabel('epoch')
     plt.show()
 
     x_test = 0*torch.ones((100,4))
@@ -120,17 +145,33 @@ if __name__ == "__main__":
     plt.hist(E,bins=30,density=True)
     plt.plot(y_test.detach(),scores.exp().detach()/denom)
     plt.title("learned Gaussian distribution")
+    plt.legend(['true','learned'])
+    plt.xlabel('e')
+    plt.ylabel('p(e)')
     plt.show()
 
-    # how to make predictions
-    yhat = X[:100,0].clone().detach()
+    # simulate test data set
+    X_test, Y_test, _ = dataGen(N_test, 1)
+    X_test = X_test/scale
+    Y_test = Y_test/scale
+
+
+    # make baseline predictions of test data set using least squares
+    estim_param, _resid, _rank, _s = linalg.lstsq(X.numpy(), Y.numpy())
+    mse_baseline = np.mean((X_test @ estim_param - Y_test) ** 2)
+
+    # make predictions of test data set using trained EBM NN
+    X_test = torch.from_numpy(X_test).float()
+    Y_test = torch.from_numpy(Y_test).float()
+
+    yhat = X_test[:,0].clone().detach()
     # yhat = torch.zeros((N-1,))
     yhat.requires_grad = True
     pred_optimizer = torch.optim.Adam([yhat], lr=0.01)
     max_steps = 1000
     #
     for step in range(max_steps):
-        score = network(X[:100,:],yhat.unsqueeze(1))
+        score = network(X_test,yhat.unsqueeze(1))
         # find the point that maximises the score
         neg_score = (-1*score).sum()
         pred_optimizer.zero_grad()
@@ -138,7 +179,15 @@ if __name__ == "__main__":
         pred_optimizer.step()
 
 
-    plt.plot(Y[:100].detach())
+    plt.plot(Y_test.detach())
     plt.plot(yhat.detach())
     plt.legend(['Meausrements','Predictions'])
+    plt.title('Test set predictions')
+    plt.xlabel('t')
+    plt.ylabel('y')
     plt.show()
+
+    mse = torch.mean((yhat - Y_test)**2)
+    print('Test MSE')
+    print('Least squares', mse_baseline)
+    print('EBM NN:', mse.item())
